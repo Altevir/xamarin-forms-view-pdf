1ae6b1c489d26f3c17bbb2df568f578b8890f62b
Altevir <altevir.cardoso@gmail.com>
2018-05-22 20:54:12 -0300

    adicionado correção pra salvar arquivos PDF em apps com API >= 24 (targetSdkVersion)

--- AppViewPDF/AppViewPDF.Android/Interfaces/PdfViewer.cs
+++ AppViewPDF/AppViewPDF.Android/Interfaces/PdfViewer.cs
@@ -1,4 +1,5 @@
 ﻿using Android.Content;
+using Android.Support.V4.Content;
 using AppViewPDF.Droid.Interfaces;
 using AppViewPDF.Interfaces;
 using System.IO;
@@ -14,10 +15,32 @@ namespace AppViewPDF.Droid.Interfaces
 
         public void ShowFile(string fileName = "", string mimeType = "")
         {
-            var uriFile = Android.Net.Uri.Parse("file://" + Path.Combine(_rootPath, fileName));
+            Android.Net.Uri uriFile = null;
             var intent = new Intent(Intent.ActionView);
+
+            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.N)
+            {
+                //Versões de API >= 24 não é mais possível expor um arquivo com file:// através de um Intent fora do seu domínio de pacote
+                //exception gerada: FileUriExposedException
+                //Exemplo de como é retornado agora: "content://com.companyname.AppViewPDF.fileprovider/external_files/Download/ArquivosPDF/XamarinFormsBook-Ch02-Apr2016.pdf"
+                //--------------------------------------------------------------------
+                //Abaixo rotina que corrige e dá acesso a gravação/leitura de arquivos
+                //Deve ser usado agora o FileProvider, o qual permite compartilhar arquivos com segurança
+                //OBS: O FileProvider tem que ser adicionado no arquivo de AndroidManifest.xml dentro de application
+                //Verificar o arquivo Resources > xml > file_paths.xml > external_files
+                //---------------------------------------------------------------------
+                intent.SetFlags(ActivityFlags.GrantWriteUriPermission | ActivityFlags.GrantReadUriPermission);
+                Java.IO.File file = new Java.IO.File(Path.Combine(_rootPath, fileName));
+                uriFile = FileProvider.GetUriForFile(Forms.Context.ApplicationContext, Forms.Context.ApplicationContext.PackageName + ".fileprovider", file);
+            }
+            else
+            {
+                uriFile = Android.Net.Uri.Parse("file://" + Path.Combine(_rootPath, fileName));
+            }
+
             intent.SetDataAndType(uriFile, mimeType);
             intent.SetFlags(ActivityFlags.ClearWhenTaskReset | ActivityFlags.NewTask);
+
             Forms.Context.StartActivity(intent);
         }
 
